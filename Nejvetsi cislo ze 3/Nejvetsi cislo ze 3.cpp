aacba24cef0c084f1746597640918353bb6af815
Kateřina Dvořáková <kacule05@gmail.com>
2021-10-31 10:43:42 +0100

    Přidejte soubory projektu.

---
+++ Nejvetsi cislo ze 3/Nejvetsi cislo ze 3.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a = 4, b = 2, c = 3;
+	printf("Jsou dana cisla %d, %d a %d\n", a, b, c);
+	int max;
+	if (a == b) {
+		max = a;
+	}
+	else {
+		if (a > b) {
+			max = a;
+		}
+		else {
+			max = b;
+		}
+	}
+	if (max == c) {
+		printf("  Nejvetsi z nich je %d\n", max);
+	}
+	else {
+		if (max > c) {
+			printf("  Nejvetsi z nich je %d\n", max);
+		}
+		else {
+			printf("  Nejvetsi z nich je %d\n", c);
+		}
+	}
+}
